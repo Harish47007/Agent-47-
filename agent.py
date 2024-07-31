1071b32f52fb8655cb07d69727324950be22e805
Harish47007 <152645206+Harish47007@users.noreply.github.com>
2024-07-31 12:37:29 +0530

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,11 @@
+"""def greet(name):
+    print(f"hello,{name}!")
+greet("Alice")
+"""
+"""import math
+result=math.sqrt(41)
+print(result)"""
+
+student = {"name": "john","age" : 25,"counters",["math","science"]}
+print(student["age"] = 26)
+student["grade"] = F
